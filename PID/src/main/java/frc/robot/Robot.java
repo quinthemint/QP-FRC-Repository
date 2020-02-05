e18055b18c777cc68c516a471bf0f94bc35f3d23
quinthemint <59623248+quinthemint@users.noreply.github.com>
2020-02-04 16:59:13 -0800

    commiiiittt

--- PID/src/main/java/frc/robot/Robot.java
+++ PID/src/main/java/frc/robot/Robot.java
@@ -1,3 +1,8 @@
+package frc.robot;
+
+import com.kauailabs.navx.frc.AHRS;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
 /*----------------------------------------------------------------------------*/
 /* Copyright (c) 2017-2019 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
@@ -5,109 +10,98 @@
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
-package frc.robot;
-
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.MedianFilter;
+import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj.controller.PIDController;
+import edu.wpi.first.wpilibj.SPI;
 
 /**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
- * project.
+ * This is a sample program to demonstrate the use of a PIDController with an
+ * ultrasonic sensor to reach and maintain a set distance from an object.
  */
 public class Robot extends TimedRobot {
-  private Command m_autonomousCommand;
+  // distance in inches the robot wants to stay from an object
+  private static final double kHoldDistance = 12.0;
 
-  private RobotContainer m_robotContainer;
+  // factor to convert sensor values to a distance in inches
+  private static final double kValueToInches = 0.125;
 
-  /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
-   */
+  // proportional speed constant
+  private static final double kP = 7.0;
+
+  // integral speed constant
+  private static final double kI = 0.018;
+
+  // derivative speed constant
+  private static final double kD = 1.5;
+
+  private Spark leftMotor1 = new Spark(0);
+  private Spark leftMotor2 = new Spark(1);
+  private Spark rightMotor1 = new Spark(2);
+  private Spark rightMotor2 = new Spark(3);
+
+  private Joystick joy1 = new Joystick(0);
+
+  private AHRS gyro = new AHRS();
+
+  // median filter to discard outliers; filters over 5 samples
+  private final MedianFilter m_filter = new MedianFilter(5);
+
+  private final PIDController m_pidController = new PIDController(kP, kI, kD);
+
+  @Override
+  public void teleopInit() {
+    // Set setpoint of the pid controller
+    m_pidController.setSetpoint(kHoldDistance * kValueToInches);
+  }
+
+  @Override
+  public void teleopPeriodic() {
+    // returned value is filtered with a rolling median filter, since ultrasonics
+    // tend to be quite noisy and susceptible to sudden outliers
+    try {
+     
+      gyro = new AHRS(SPI.Port.kMXP); 
+  } catch (RuntimeException ex ) {
+      DriverStation.reportError("Error instantiating navX-MXP:  " + ex.getMessage(), true);
+  }
+
+  try {
+    
+    Robot.mecanumDrive_Cartesian(joy1.getX(), joy1.getY(), 
+                                currentRotationRate, gyro.getAngle());
+} catch( RuntimeException ex ) {
+    DriverStation.reportError("Error communicating with drive system:  " + ex.getMessage(), true);
+}
+
+    leftMotor1.set(pidOutput);
+    leftMotor2.set(pidOutput);
+    rightMotor1.set(pidOutput);
+    rightMotor2.set(pidOutput);
+  }
+
   @Override
   public void robotInit() {
-    // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
-    // autonomous chooser on the dashboard.
-    m_robotContainer = new RobotContainer();
   }
 
-  /**
-   * This function is called every robot packet, no matter the mode. Use this for items like
-   * diagnostics that you want ran during disabled, autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before
-   * LiveWindow and SmartDashboard integrated updating.
-   */
-  @Override
-  public void robotPeriodic() {
-    // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
-    // commands, running already-scheduled commands, removing finished or interrupted commands,
-    // and running subsystem periodic() methods.  This must be called from the robot's periodic
-    // block in order for anything in the Command-based framework to work.
-    CommandScheduler.getInstance().run();
-  }
-
-  /**
-   * This function is called once each time the robot enters Disabled mode.
-   */
-  @Override
-  public void disabledInit() {
-  }
-
-  @Override
-  public void disabledPeriodic() {
-  }
-
-  /**
-   * This autonomous runs the autonomous command selected by your {@link RobotContainer} class.
-   */
   @Override
   public void autonomousInit() {
-    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
-
-    // schedule the autonomous command (example)
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.schedule();
-    }
   }
 
-  /**
-   * This function is called periodically during autonomous.
-   */
   @Override
   public void autonomousPeriodic() {
   }
 
   @Override
-  public void teleopInit() {
-    // This makes sure that the autonomous stops running when
-    // teleop starts running. If you want the autonomous to
-    // continue until interrupted by another command, remove
-    // this line or comment it out.
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.cancel();
-    }
-  }
-
-  /**
-   * This function is called periodically during operator control.
-   */
-  @Override
-  public void teleopPeriodic() {
-  }
-
-  @Override
   public void testInit() {
-    // Cancels all running commands at the start of test mode.
-    CommandScheduler.getInstance().cancelAll();
   }
 
-  /**
-   * This function is called periodically during test mode.
-   */
   @Override
   public void testPeriodic() {
   }
+
 }
